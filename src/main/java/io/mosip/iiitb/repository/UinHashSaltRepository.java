8aa2cfcf0a399a8c1c9f44fbb27dfba0e5d48140
abdulbathish <arbathish@noreply.github.com>
2024-06-01 01:40:13 +0530

    cache the hash salt values at startup

--- src/main/java/io/mosip/iiitb/repository/UinHashSaltRepository.java
+++ src/main/java/io/mosip/iiitb/repository/UinHashSaltRepository.java
@@ -6,18 +6,25 @@ import io.mosip.iiitb.entity.UinHashSaltEntity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+import org.slf4j.Logger;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UinHashSaltRepository {
+    private final Logger logger;
     private final EntityManagerFactory emf;
     private final EntityManager em;
+    private final UinHashSaltEntity[] dbValuesCache;
 
     @Inject
     public UinHashSaltRepository(
-            OnDemandAppConfig config
+            OnDemandAppConfig config,
+            Logger loggerArg
     ) {
+        logger = loggerArg;
         Map<String, String> properties = new HashMap<>();
         properties.put("jakarta.persistence.jdbc.url", config.dbUrl());
         properties.put("jakarta.persistence.jdbc.user", config.dbUsername());
@@ -28,9 +35,14 @@ public class UinHashSaltRepository {
                 properties
         );
         em = emf.createEntityManager();
+        dbValuesCache = new UinHashSaltEntity[1000];
+        initCache();
     }
 
     public UinHashSaltEntity findById(int id) {
+        if (id > -1 && id < 1000) {
+            return dbValuesCache[id];
+        }
         return em.find(UinHashSaltEntity.class, id);
     }
 
@@ -41,5 +53,26 @@ public class UinHashSaltRepository {
         if (emf != null && emf.isOpen()) {
             emf.close();
         }
+    }
+
+    private void initCache() {
+        logger.debug("Initializing Cache");
+        TypedQuery<UinHashSaltEntity> query = em.createQuery(
+                "SELECT u FROM UinHashSaltEntity u",
+                UinHashSaltEntity.class
+        );
+        List<UinHashSaltEntity> results = query.getResultList();
+        int cached_count = 0;
+        for (UinHashSaltEntity record: results) {
+            int id = record.getId();
+            if (id < 1000) {
+                ++cached_count;
+                dbValuesCache[id] = record;
+            }
+        }
+        logger.debug(
+                "Caching completed, Cached {} entries",
+                cached_count
+        );
     }
 }
