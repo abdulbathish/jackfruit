482c3af7a0da0a457b9591bfe118338c23d9e7f8
abdulbathish <arbathish@noreply.github.com>
2024-06-05 17:31:44 +0530

    typo fix

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ DOCKER_IMAGE="abdulbathish/mosip-ondemand:${TAG_VERSION}"
 BUILD_TIME=$(date --iso-8601=seconds)
 COMMIT_ID=$(git rev-list --abbrev-commit -n 1 ${TAG_VERSION})
 
-mvn relase:prepare
+#mvn release:prepare
 if ! docker image inspect "${DOCKER_IMAGE}" >/dev/null 2>&1
 then
   "${DOCKER_CMD}" build --build-arg COMMIT_ID=${COMMIT_ID} --build-arg BUILD_TIME="${BUILD_TIME}" --build-arg TAG_VERSION="${TAG_VERSION}" -t "${DOCKER_IMAGE}" .
