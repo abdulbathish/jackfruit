00cbea3a6c9e47e54f5322ad6e138fba27602c91
abdulbathish <arbathish@noreply.github.com>
2024-06-06 14:25:38 +0530

    mvn command before querying tag version

--- release.sh
+++ release.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
+mvn release:prepare
+
 DOCKER_CMD="/usr/bin/docker"
 TAG_VERSION=$(git describe --tags --abbrev=0)
 DOCKER_IMAGE="abdulbathish/mosip-ondemand:${TAG_VERSION}"
 BUILD_TIME=$(date --iso-8601=seconds)
 COMMIT_ID=$(git rev-list --abbrev-commit -n 1 ${TAG_VERSION})
 
-mvn release:prepare
 if ! docker image inspect "${DOCKER_IMAGE}" >/dev/null 2>&1
 then
   "${DOCKER_CMD}" build --build-arg COMMIT_ID=${COMMIT_ID} --build-arg BUILD_TIME="${BUILD_TIME}" --build-arg TAG_VERSION="${TAG_VERSION}" -t "${DOCKER_IMAGE}" .
