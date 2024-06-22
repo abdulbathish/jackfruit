b158d78acb594bd82edb1fc2470cc8fee19fb590
abdulbathish <arbathish@noreply.github.com>
2024-06-22 12:55:51 +0530

    RSACryptoTool: helper function to encrypt data, so its easier to test

--- src/main/java/io/mosip/iiitb/utils/RSACryptoTool.java
+++ src/main/java/io/mosip/iiitb/utils/RSACryptoTool.java
@@ -4,7 +4,9 @@ import com.google.inject.Inject;
 import io.mosip.iiitb.config.OnDemandAppConfig;
 import org.slf4j.Logger;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.OAEPParameterSpec;
 import javax.crypto.spec.PSource;
@@ -16,6 +18,7 @@ import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.MGF1ParameterSpec;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 public class RSACryptoTool {
@@ -38,41 +41,66 @@ public class RSACryptoTool {
             this.cipher = cipher;
         } catch (NoSuchAlgorithmException ex) {
             logger.error(ex.getMessage());
-            System.exit(1);
+            System.exit(11);
         } catch (NoSuchPaddingException ex) {
             logger.error(ex.getMessage());
-            System.exit(1);
+            System.exit(12);
         } catch (InvalidKeyException ex) {
             logger.error(ex.getMessage());
-            System.exit(1);
+            System.exit(13);
         } catch (InvalidAlgorithmParameterException ex) {
             logger.error(ex.getMessage());
-            System.exit(1);
+            System.exit(14);
         }
     }
     public byte[] decryptData(byte[] encryptedData) throws Exception {
         return this.cipher.doFinal(encryptedData);
     }
 
-    private PrivateKey loadPrivateKey(String privateKeyPath) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        Path privateKeyLocation = Paths.get(privateKeyPath);
-        String pkContents = "";
+    public byte[] encryptData(
+            byte[] data,
+            String publicKeyPath
+    ) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        PublicKey pubKey = loadPublicKey(publicKeyPath);
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+        OAEPParameterSpec oaepParams = new OAEPParameterSpec("SHA-256", "MGF1", MGF1ParameterSpec.SHA256, PSource.PSpecified.DEFAULT);
+        cipher.init(Cipher.ENCRYPT_MODE, pubKey, oaepParams);
+        byte[] encrypted = cipher.doFinal(data);
+        return encrypted;
+    }
+
+    private String readKeyContents(String keyPath) {
+        Path privateKeyLocation = Paths.get(keyPath);
         try {
             byte[] pkContentsRaw = Files.readAllBytes(privateKeyLocation);
-            pkContents = new String(pkContentsRaw)
+            String pkContents = new String(pkContentsRaw)
                     .replaceAll("\\n", "")
                     .replaceAll("-----BEGIN PRIVATE KEY-----", "")
+                    .replaceAll("-----BEGIN PUBLIC KEY-----", "")
+                    .replaceAll("-----END PUBLIC KEY-----", "")
                     .replaceAll("-----END PRIVATE KEY-----", "");
+            return pkContents;
         } catch (IOException ex) {
             this.logger.error(
-                    String.format(
-                            "Likely failed to locate private key in given path.\nPath = %s ",
-                            privateKeyPath
-                    )
+                    "Likely failed to locate key in given path.\nPath = {} ",
+                    keyPath
             );
             System.exit(30);
         }
+        return "";
+    }
 
+    private PublicKey loadPublicKey(String publicKeyPath) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String keyContents = readKeyContents(publicKeyPath);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(
+                Base64.getDecoder().decode(keyContents)
+        );
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return kf.generatePublic(spec);
+    }
+
+    private PrivateKey loadPrivateKey(String privateKeyPath) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String pkContents = readKeyContents(privateKeyPath);
         PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(
             Base64.getDecoder().decode(pkContents)
         );
