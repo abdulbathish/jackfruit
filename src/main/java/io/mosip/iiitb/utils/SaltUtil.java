7735c1e3e26dcd768a4b1cc9f6051c020c529287
abdulbathish <arbathish@noreply.github.com>
2024-07-09 11:14:08 +0530

    new change

--- src/main/java/io/mosip/iiitb/utils/SaltUtil.java
+++ src/main/java/io/mosip/iiitb/utils/SaltUtil.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import io.mosip.iiitb.entity.UinHashSaltEntity;
 import io.mosip.iiitb.config.OnDemandAppConfig;
 import io.mosip.iiitb.repository.UinHashSaltRepository;
+import io.mosip.iiitb.utils.HMACUtil2;
 import io.mosip.kernel.core.util.HMACUtils2;
 import org.slf4j.Logger;
 
@@ -27,17 +28,34 @@ public class SaltUtil {
         this.config = config;
         this.Length = config.saltUtilLen();
     }
-    public String getSaltForUid(String uid) {
-        String salt;
+
+
+    public String getSaltForVid(String vid) {
         try {
-            int maxLen = Length;
-            int modulo = calculateModulo(uid, maxLen);
-            salt = getSaltFromDB(modulo);
+            int maxLen = 3;
+            int modulo = calculateModulo(vid, maxLen);
+            String salt = getSaltFromDB(modulo);
         } catch (NoSuchAlgorithmException e) {
-            logger.debug("Failed to calculate modulo due to missing algorithm: ", e);
+            LOGGER.log(Level.SEVERE, "Failed to calculate modulo due to missing algorithm: ", e);
             return null;
         }
-        return salt;
+    }
+
+    private int calculateModulo(String input, int maxLength) throws NoSuchAlgorithmException {
+        String hash = HMACUtils2.digestAsPlainText(input.getBytes());
+        int hexToDecimal = convertSubstringToInt(hash, maxLength, 16);
+        String decimalStr = String.valueOf(hexToDecimal);
+        return convertSubstringToInt(decimalStr, maxLength, 10);
+    }
+
+    private int convertSubstringToInt(String input, int maxLength, int radix) {
+        String substring = extractSubstring(input, maxLength);
+        return Integer.parseInt(substring, radix);
+    }
+
+    private String extractSubstring(String input, int maxLength) {
+        int length = input.length();
+        return length > maxLength ? input.substring(length - maxLength) : input;
     }
 
     private int calculateModulo(String input, int maxLength) throws NoSuchAlgorithmException {
