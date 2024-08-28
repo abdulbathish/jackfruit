8cfdf6153d88dafb170e7307cb4f74d1673b1275
abdulbathish <arbathish@noreply.github.com>
2024-08-28 14:08:26 +0530

    event consumer: remove unused commennt

--- src/main/java/io/mosip/iiitb/odte/consumers/EventConsumer.java
+++ src/main/java/io/mosip/iiitb/odte/consumers/EventConsumer.java
@@ -2,5 +2,4 @@ package io.mosip.iiitb.odte.consumers;
 
 abstract public interface EventConsumer<T> {
     public EventConsumerResponse processRecord(T record);
-    // public EventConsumerResponse processRecord(String record);
 }
